5e25e12e0212e9fceb7b19c5a893361b9ca9245b
eundin <escience1202@gmail.com>
2024-12-30 09:49:32 +0900

    test

--- src/app/query/page.tsx
+++ src/app/query/page.tsx
@@ -15,7 +15,7 @@ export default Query;
 
 export const noEmptyString = (str: string) => {
   if (str === "") {
-    throw new Error("빈 문자열이 들어왔습니다ee.");
+    throw new Error("빈 문자열이 들어왔습니다eed.");
   }
 
   return str;
